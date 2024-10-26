024e313421621be954656d375a54805a6fead92a
Yash698869 <161454770+Yash698869@users.noreply.github.com>
2024-10-27 01:09:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,74 @@
 let userScore = 0;
 let compScore = 0;
 
-const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg");
+const choic = document.querySelectorAll(".choice")
+let msg = document.querySelector("#msg")
 
-const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score");
+userScorePara = document.querySelector("#user-score")
+compScorePara = document.querySelector("#comp-score")
 
-const genCompChoice = () => {
-    const options = ["rock", "paper", "scissors"];
-    const randIdx = Math.floor(Math.random() * 3);
-    return options[randIdx];
-};
 
-const drawGame = () => {
-    msg.innerText = "Game was Draw. Play again.";
-    msg.style.backgroundColor = "#081b31";
-};
+const gencompChoice = ()=>{
+    const options = ["rock" , "paper" , "scissor"];
+    const rdnIdx = Math.floor(Math.random()*3)
+    return options[rdnIdx];
+}
 
-const showWinner = (userWin, userChoice, compChoice) => {
-    if (userWin) {
+const drawGame = ()=>{
+    console.log("Game is draw please play again")
+    msg.innerText = "Game Draw"
+    msg.style.backgroundColor = "#081b31"
+}
+
+const showWinner =(userWin,userChoice,compChoice)=>{
+    if(userWin){// if userWin is still true then user has won
         userScore++;
-        userScorePara.innerText = userScore;
-        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
-        msg.style.backgroundColor = "green";
-    } else {
-        compScore++;
-        compScorePara.innerText = compScore;
-        msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
-        msg.style.backgroundColor = "red";
+        userScorePara.innerHTML= userScore;
+        msg.innerText = ` You Win! Your${userChoice} beats ${compChoice}`
+        msg.style.backgroundColor = "green"
+
     }
-};
+    else{
+        compScore++
+        compScorePara.innerHTML = compScore;
+        msg.innerText = ` You lose! ${compChoice} beats Your${userChoice}`
+        msg.style.backgroundColor = "red"
 
-const playGame = (userChoice) => {
-    //Generate computer choice
-    const compChoice = genCompChoice();
 
-    if (userChoice === compChoice) {
-        //Draw Game
-        drawGame();
-    } else {
-        let userWin = true;
-        if (userChoice === "rock") {
-            //scissors, paper
-            userWin = compChoice === "paper" ? false : true;
-        } else if (userChoice === "paper") {
-            //rock, scissors
-            userWin = compChoice === "scissors" ? false : true;
-        } else {
-            //rock, paper
-            userWin = compChoice === "rock" ? false : true;
+    }
+}
+
+const playGame = (userChoice)=>{
+console.log("User choice =",userChoice);
+
+    const compChoice = gencompChoice();
+    console.log("Comp choice =",compChoice);
+
+    if(userChoice === compChoice){
+drawGame();
+    }
+    else{
+        let userWin = true ;
+        if(userChoice === "rock"){ // toh comp wala rock nhi choose kar sakta nhi toh draw ho jayega upper wala method
+            userWin = compChoice ==="paper"?false : true;
         }
-        showWinner(userWin, userChoice, compChoice);
+        else if(userChoice === "paper"){
+            // rock or scissor
+            userWin = compChoice === "scissor"?false : true;
+        }
+        else{
+            // rock or paper == scissor ke liye h
+            userWin = compChoice === "rock"?false:true;
+        }
+        showWinner(userWin,userChoice,compChoice);
     }
-};
+}
 
-choices.forEach((choice) => {
-    choice.addEventListener("click", () => {
-        const userChoice = choice.getAttribute("id");
+
+
+choic.forEach((choice)=>{
+     let userChoice = choice.getAttribute("id")
+    choice.addEventListener("click",()=>{
         playGame(userChoice);
-    });
-});
+    })
+})
